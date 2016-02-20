48e012da441724e4cc3ee18e1ef995c61d90bce3
pro <pro@pros-Mac-mini.local>
2016-02-20 20:33:11 +0100

    2nd

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,7 @@ namespace empty7
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
+            //app.UseIISPlatformHandler();
 
             app.UseMvcWithDefaultRoute();
         }
